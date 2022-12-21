7e636e8fb2d182ba838f5b0d77a753daba5f4bb8
zulqernayn <zulqernaynh@gmail.com>
2022-12-21 16:45:50 +0500

    .,.,.

--- config/fmpObjs.js
+++ config/fmpObjs.js
@@ -1,18 +1,18 @@
-const login = {
-  event: "login",
-  data: {
-    apiKey: process.env.API_KEY,
-  },
-};
+// const login = {
+//   event: "login",
+//   data: {
+//     apiKey: process.env.API_KEY,
+//   },
+// };
 
-const subscribe = {
-  event: "subscribe",
-  data: {
-    ticker: "btcusd",
-  },
-};
+// const subscribe = {
+//   event: "subscribe",
+//   data: {
+//     ticker: "btcusd",
+//   },
+// };
 
-module.exports={
-    login,
-    subscribe
-}
+// module.exports={
+//     login,
+//     subscribe
+// }
