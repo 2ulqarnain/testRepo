4a43604deaaae2c9ea7e89b287e4251ff76433d9
zulqernayn <zulqernaynh@gmail.com>
2022-12-21 16:45:05 +0500

    ...

--- utils/handlers.js
+++ utils/handlers.js
@@ -1,10 +1,12 @@
-const { ws } = require("../config/connectToFMP");
-
 const handleOnClientConnected = socket => {
 
     console.log("socket.id")
-  
-    ws.on("message", (data) => socket.emit("SOC_DATA",data));
+
+    socket.emit("customEvent","customData1")
+    socket.emit("customEvent","customData2")
+    socket.emit("customEvent","customData3")
+    socket.emit("customEvent","customData4")
+    socket.emit("customEvent","customData5")
   
     socket.on("disconnect", () => {
       console.info(`Client gone [id=${socket.id}]`);
